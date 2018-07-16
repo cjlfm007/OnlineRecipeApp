5e872bb044a5596cea7cf4b56ad35d1b95f4322e
Fangming Lu <cjlfm007@gmail.com>
2018-07-16 02:23:50 -0700

    first api call

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
-// Global app controller
+/*import str from './models/Search'
+//import { add as a, multiply as m,ID} from './views/searchView';
+import * as searchView from './views/searchView'
+//console.log(`Using imported functions! ${a(ID,2)} and ${m(3,5)}.${str}}`);
+console.log(`Using imported functions! ${searchView.add(searchView.ID,2)} and ${searchView.multiply(3,5)}.${str}}`);*/
+//3535bebde6997633c11f1d49881788cd
+//http://food2fork.com/api/search 
+import axios from 'axios';
+
+async function getResults(query) {
+    //fetch
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '3535bebde6997633c11f1d49881788cd';
+    try{
+    const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+    const recipes = res.data.recipes;
+    console.log(recipes);
+    }catch(error){
+        alert(error);
+    }
+
+}
+getResults('pizza');

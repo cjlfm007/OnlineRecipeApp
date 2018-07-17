d95bb2613d51bbae795c3c6cf3202e50f0da57c0
Fangming Lu <cjlfm007@gmail.com>
2018-07-17 02:09:21 -0700

    add spinner

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,7 @@ console.log(`Using imported functions! ${searchView.add(searchView.ID,2)} and ${
 
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import {elements} from './views/base';
+import {elements, renderLoader,clearLoader} from './views/base';
 /**Global state of the app
  * -Search object
  * -Current recipe object
@@ -29,12 +29,14 @@ const controlSearch = async () => {
         // 3) Prepare UI for results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         // 4) Search for recipes;
         await state.search.getResults();// 所有async method return 的都是promise，因为getResults定义的是async method所以这里也是返回的promise，所以需要await
 
         // 5) Render results on UI
         //console.log(state.search.result);
+        clearLoader();
         searchView.renderResults(state.search.result);
     }
 }
